af055758f566a9bb5110f30c7841dba40f6b7b7f
Waris Thongpho <yosyosyos100@gmail.com>
2024-09-02 11:11:40 +0700

    context content

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import React from 'react';
-import MessageBox from './state-func';
-
-function App() {
-  return (
-    <MessageBox></MessageBox>
-  );
+import { userContext } from './context';
+import Header2 from './context-header2'
+import Content2 from './context-content2'
+ 
+export default function App(){
+  let[user,setUser]=React.useState('')
+  return(
+    <userContext.Provider value={[user,setUser]}>
+      <Header2/>
+      <Content2/>
+    </userContext.Provider>
+  )
 }
-
-export default App;
-
